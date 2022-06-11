1aaa088aefdcd3324319701c0ffacec9615d5706
shiba inu <jefferson121996@gmail.com>
2022-06-11 18:13:07 -0400

    get one product by its id

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -33,7 +33,31 @@ router.get('/', (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
   // be sure to include its associated Category and Tag data
+  include:
+  [
+    {
+      model: Category,
+      attributes: ['id', 'category_name']
+    },
+    {
+      model: Tag,
+      attributes: ['id', 'tag_name'],
+    }
+  ]
+  })
+  .then(getProductById => {
+    res.json(getProductById)
+    .status(200);
+  })  
+  .catch(err => {
+    console.error(`Unexpected error: ${err}`);
+    res.status(500);
+  })
 });
 
 // create new product
